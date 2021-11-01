7fd1ec58664b4b42cfb139a5c880348ae65e6edc
Bogdan <b.r.protsenko@student.khai.edu>
2021-11-01 14:59:12 +0200

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+    public  static void main(String[] args) {
+        System.out.println("Hello! My name is BOT " +
+                "\nI was created in 2021");
+    }
+}
