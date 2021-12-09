00381f7c208af81c26ce5679701aeb7be5be6e0a
Bogdan <b.r.protsenko@student.khai.edu>
2021-12-09 14:18:58 +0200

    CreditCalculator Project - Stage 2

--- src/CreditCalculator.java
+++ src/CreditCalculator.java
@@ -1,12 +1,41 @@
+import java.text.DecimalFormat;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.*;
+
 public class CreditCalculator {
     public static void main(String[] args) {
         calculator();
     }
-    static void calculator(){
-        System.out.println("Loan principal: 1000\n" +
-                "Month 1: repaid 250\n" +
-                "Month 2: repaid 250\n" +
-                "Month 3: repaid 500\n" +
-                "The loan has been repaid!");
+    public static void calculator() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the loan principal:");
+        int principal = scanner.nextInt();
+        System.out.println("What do you want to calculate?\n"
+                + "type 'm' – for number of monthly payments,\n"
+                + "type 'p' – for the monthly payment:");
+        pay(principal);
+    }
+    public static   void pay(int principal){
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        Scanner scanner1 = new Scanner(System.in);
+            String type = scanner1.nextLine();
+            if(Objects.equals(type,"m")){
+                System.out.println("Enter the monthly payment:");
+                int monthly_payment = scanner1.nextInt();
+                System.out.println("It will take " +principal/monthly_payment + " months to repay the loan");
+            }
+            if(Objects.equals(type,"p")){
+                System.out.println("Enter the number of months:");
+                int periods = scanner1.nextInt();
+                float lastpayment = (float) (principal-((periods-1)*( Math.ceil((double)principal/periods))));
+                if(principal%periods == 0){
+                    System.out.println("Your monthly payment =" + principal/periods);
+                }
+                if(principal%periods > 0){
+                    System.out.println("Your monthly payment = " + Math.ceil((double)principal/periods) + " and the last payment = "+lastpayment);
+                }
+            }
+
     }
 }
