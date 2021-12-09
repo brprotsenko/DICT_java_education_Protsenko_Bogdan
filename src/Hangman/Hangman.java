118be6bd2a955057935412789ba5329c8807c0da
Bogdan <b.r.protsenko@student.khai.edu>
2021-12-09 12:03:02 +0200

    Hangman Project - Stage 1

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,7 +1,23 @@
 package Hangman;
 
-public class Hangman {
-    public static void main(String[] args) {
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Hangman {
+    public void play(){
+        menu();
+    }
+    public void menu(){
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 1; i>i-1;i++){
+            System.out.println("HANGMAN");
+            System.out.print("Type play to play the game,exit to quit:> ");
+            String answer = scanner.nextLine();
+            if(Objects.equals(answer,"play")){
+                words();
+            }else {
+                break;
+            }
+        }
     }
 }
