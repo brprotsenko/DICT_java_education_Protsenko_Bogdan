65c8fe4fb82adb45dde9be505ee81ac3f91f6c5c
Bogdan <b.r.protsenko@student.khai.edu>
2021-12-09 12:24:24 +0200

    TicTacToe Project - Stage 1

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,17 @@
 public class TicTacToe {
     public static void main(String[] args) {
-
+        play();
+    }
+    static void play(){
+        playingfield();
+    }
+    static void playingfield(){
+        String[][] field = new String[5][5];
+        for(int i = 0;i<5;i++){
+            for (int j = 0;j<5;j++){
+                field[i][j] = "_";
+            }
+        }
+        tictactoe(field);
     }
 }
